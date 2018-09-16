cd363f4f0f456de791396f9c3237ee3af341d4bc
nizhihuahome <116778012@qq.com>
2018-09-16 21:28:29 +0800

    添加了cube素材

---
+++ Assets/KUBIKOS - World/Demo/Scripts/LoadSceneByName.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Animmal.Kubikos
+{
+    public class LoadSceneByName : MonoBehaviour
+    {
+        public string SceneName;
+
+        public void LoadLevel()
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(SceneName);
+        }
+    }
+}
