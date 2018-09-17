237145ae5c18f9fe6be00f51f1c8dd6a3be8ad69
nizhi <116778012@qq.com>
2018-09-17 16:50:31 +0800

    添加odim插件和加入element引擎
    添加了odim编辑器插件（2.01），添加了元素引擎（ElementEngine）的部分相关代码

---
+++ Assets/Scripts/Runtime/ElementEngine/ElementObject.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 基本化学元素类型基类
+/// </summary>
+public class ElementObject : MonoBehaviour {
+    /// <summary>
+    /// 水元素值
+    /// </summary>
+    public int water;
+    /// <summary>
+    /// 水元素抗性
+    /// </summary>
+    public int waterDefense;
+    /// <summary>
+    /// 火元素值
+    /// </summary>
+    public int fire;
+    /// <summary>
+    /// 火元素抗性
+    /// </summary>
+    public int fireDefense;
+    /// <summary>
+    /// 电元素
+    /// </summary>
+    public int electric;
+    /// <summary>
+    /// 电元素抗性
+    /// </summary>
+    public int electricDefense;
+    /// <summary>
+    /// 病毒值
+    /// </summary>
+    public int virus;
+    /// <summary>
+    /// 病毒抗性
+    /// </summary>
+    public int virusDefense;
+    /// <summary>
+    /// 鲜血值
+    /// </summary>
+    public int blood;
+    /// <summary>
+    /// 鲜血抗性
+    /// </summary>
+    public int bloodDefense;
+
+    public List<ElementProperty> properties=new List<ElementProperty>();
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Runtime/ElementEngine/ElementProperty.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct ElementProperty  {
+    public string name;
+    public float value;
+    public float defense;
+    public bool enable;
+    //public ElementProperty(string setName)
+    //{
+    //    this.name = setName;
+    //    this.enable = true;
+    //}
+}
