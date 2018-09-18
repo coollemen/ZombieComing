d3c026fc3620f38b51f9f501e298f37c7d8dccef
nizhihuahome <116778012@qq.com>
2018-09-18 21:44:39 +0800

    添加了高亮插件

---
+++ Assets/Scripts/Runtime/Outline.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using HighlightingSystem;
+using UnityEngine;
+public class Outline : MonoBehaviour
+{
+    private Highlighter h;
+    // Use this for initialization
+    void Start ()
+    {
+        h = GetComponent<Highlighter>();
+    }
+
+   public  void OnTap()
+    {
+        // Fade in constant highlighting  
+        h.ConstantOn(Color.red);
+
+        // Turn off constant highlighting  
+        //h.ConstantOffImmediate();
+
+        // Start flashing from blue to cyan color and frequency = 2f  
+        //h.FlashingOn(Color.blue, Color.cyan, 2f);
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
