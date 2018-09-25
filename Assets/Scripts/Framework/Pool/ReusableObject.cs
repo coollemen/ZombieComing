2c95854f97910e265b354682443c403d814a4998
nizhihuahome <116778012@qq.com>
2018-09-25 21:13:33 +0800

    删除多余代码

--- Assets/Scripts/Framework/Pool/ReusableObject.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class ReusableObject : MonoBehaviour, IReusable
-{
-    public abstract void OnSpawn();
-    public abstract void OnUnspawn();
-}
