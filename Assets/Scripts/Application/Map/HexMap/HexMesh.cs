69e53a9a0ad97852fcf97b71cd53e0bb79ab6790
nizhi <116778012@qq.com>
2018-09-29 16:50:22 +0800

    添加了六边形地图

---
+++ Assets/Scripts/Application/Map/HexMap/HexMesh.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class HexMesh : MonoBehaviour
+{
+
+    Mesh hexMesh;
+    List<Vector3> vertices;
+    List<int> triangles;
+    MeshCollider meshCollider;
+    List<Color> colors;
+    void Awake()
+    {
+        GetComponent<MeshFilter>().mesh = hexMesh = new Mesh();
+        hexMesh.name = "Hex Mesh";
+        vertices = new List<Vector3>();
+        triangles = new List<int>();
+        colors = new List<Color>();
+        meshCollider = gameObject.AddComponent<MeshCollider>();
+    }
+    public void Triangulate(HexCell[] cells)
+    {
+        hexMesh.Clear();
+        vertices.Clear();
+        triangles.Clear();
+        colors.Clear();
+        for (int i = 0; i < cells.Length; i++)
+        {
+            Triangulate(cells[i]);
+        }
+        hexMesh.vertices = vertices.ToArray();
+        hexMesh.triangles = triangles.ToArray();
+        hexMesh.colors = colors.ToArray();
+        hexMesh.RecalculateNormals();
+        meshCollider.sharedMesh = hexMesh;
+    }
+
+    void Triangulate(HexCell cell)
+    {
+        Vector3 center = cell.transform.localPosition;
+        for (int i = 0; i < 6; i++)
+        {
+            AddTriangle(
+                center,
+                center + HexMetrics.corners[i],
+                center + HexMetrics.corners[i + 1]
+            );
+            AddTriangleColor(cell.color);
+        }
+    }
+    void AddTriangle(Vector3 v1, Vector3 v2, Vector3 v3)
+    {
+        int vertexIndex = vertices.Count;
+        vertices.Add(v1);
+        vertices.Add(v2);
+        vertices.Add(v3);
+        triangles.Add(vertexIndex);
+        triangles.Add(vertexIndex + 1);
+        triangles.Add(vertexIndex + 2);
+    }
+    void AddTriangleColor(Color color)
+    {
+        colors.Add(color);
+        colors.Add(color);
+        colors.Add(color);
+    }
+}
---
+++ Assets/Scripts/Application/Map/HexMap/HexMetrics.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class HexMetrics  {
+    /// <summary>
+    /// 外径
+    /// </summary>
+    public const float outerRadius = 10f;
+    /// <summary>
+    ///内径
+    /// </summary>
+    public const float innerRadius = outerRadius * 0.866025404f;
+
+    public static Vector3[] corners = {
+        new Vector3(0f, 0f, outerRadius),
+        new Vector3(innerRadius, 0f, 0.5f * outerRadius),
+        new Vector3(innerRadius, 0f, -0.5f * outerRadius),
+        new Vector3(0f, 0f, -outerRadius),
+        new Vector3(-innerRadius, 0f, -0.5f * outerRadius),
+        new Vector3(-innerRadius, 0f, 0.5f * outerRadius),
+        new Vector3(0f, 0f, outerRadius)
+    };
+
+}
