797ce50b0a3216d1fbd3ffdc1ce1717a8cf69e07
nizhihuahome <116778012@qq.com>
2018-09-29 21:46:42 +0800

    添加了四边形图块代码

---
+++ Assets/Scripts/Application/Map/QuadMap/QuadGrid.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuadGrid : MonoBehaviour
+{
+    public int width=400;
+    public int height=300;
+    public QuadCell cellPrefab;
+    public List<QuadCell> cells = new List<QuadCell>();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
