8cf59bd73b3b379064916f9f00c31f8e899c576e
nizhihuahome <116778012@qq.com>
2018-11-08 21:37:55 +0800

    chunk基础功能完成

--- Assets/GameFramework/6.Map/Chunk.cs
+++ Assets/GameFramework/6.Map/Chunk.cs
@@ -10,24 +10,9 @@ namespace GameFramework
     [RequireComponent(typeof(MeshCollider))]
     public class Chunk : MonoBehaviour
     {
-        public static int width = 16;
-        public static int height = 16;
-
-        public byte[,,] blocks;
-        public Vector3Int position;
 
         private Mesh mesh;
         public Section[] sections = new Section[16];
-        //面需要的点
-        private List<Vector3> vertices = new List<Vector3>();
-        //生成三边面时用到的vertices的index
-        private List<int> triangles = new List<int>();
-        //所有的uv信息
-        private List<Vector2> uv = new List<Vector2>();
-        //uv贴图每行每列的宽度(0~1)，这里我的贴图是32×32的，所以是1/32
-        public static float textureOffset = 1 / 32f;
-        //让UV稍微缩小一点，避免出现它旁边的贴图
-        public static float shrinkSize = 0.001f;
 
 
         //当前Chunk是否正在生成中
@@ -35,300 +20,47 @@ namespace GameFramework
 
         void Start()
         {
-            position = new Vector3Int((int)this.transform.position.x, (int)this.transform.position.y, (int)this.transform.position.z);
-//            if (Map.instance.ChunkExists(position))
-//            {
-//                Debug.Log("此方块已存在" + position);
-//                Destroy(this);
-//            }
-//            else
-//            {
-//                Map.instance.chunks.Add(position, this.gameObject);
-//                this.name = "(" + position.x + "," + position.y + "," + position.z + ")";
-//                StartFunction();
-//            }
-            //StartFunction();
+            StartCoroutine(CreateChunkMesh());
         }
 
-
-        void StartFunction()
-        {
-            mesh = new Mesh();
-            mesh.name = "Chunk";
-
-            StartCoroutine(CreateMap());
-        }
-
-        IEnumerator CreateMap()
+        IEnumerator CreateChunkMesh()
         {
             while (isWorking)
             {
                 yield return null;
             }
             isWorking = true;
-            blocks = new byte[width, height, width];
-            for (int x = 0; x < Chunk.width; x++)
+            mesh = new Mesh();
+            mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+            mesh.name = "chunk";
+            for (int i = 0; i < sections.Length; i++)
             {
-                for (int y = 0; y < Chunk.height; y++)
-                {
-                    for (int z = 0; z < Chunk.width; z++)
-                    {
-                        if (y == Chunk.height - 1)
-                        {
-                            if (Random.Range(1, 5) == 1)
-                            {
-                                blocks[x, y, z] = 2;
-                            }
-                        }
-                        else
-                        {
-                            blocks[x, y, z] = 1;
-                        }
-                    }
-                }
+                sections[i] = new Section(i);
+               yield return StartCoroutine(sections[i].CreateMesh());
             }
-
-            StartCoroutine(CreateMesh());
-        }
-
-        IEnumerator CreateMesh()
-        {
-            vertices.Clear();
-            triangles.Clear();
-
-            //把所有面的点和面的索引添加进去
-            for (int x = 0; x < Chunk.width; x++)
+            CombineInstance[] instances = new CombineInstance[sections.Length];
+            for (int i = 0; i < sections.Length; i++)
             {
-                for (int y = 0; y < Chunk.height; y++)
-                {
-                    for (int z = 0; z < Chunk.width; z++)
-                    {
-                        //获取当前坐标的Block对象
-                        Block block = BlockList.GetBlock(this.blocks[x, y, z]);
-                        if (block == null) continue;
-
-                        if (IsBlockTransparent(x + 1, y, z))
-                        {
-                            AddFrontFace(x, y, z, block);
-                        }
-                        if (IsBlockTransparent(x - 1, y, z))
-                        {
-                            AddBackFace(x, y, z, block);
-                        }
-                        if (IsBlockTransparent(x, y, z + 1))
-                        {
-                            AddRightFace(x, y, z, block);
-                        }
-                        if (IsBlockTransparent(x, y, z - 1))
-                        {
-                            AddLeftFace(x, y, z, block);
-                        }
-                        if (IsBlockTransparent(x, y + 1, z))
-                        {
-                            AddTopFace(x, y, z, block);
-                        }
-                        if (IsBlockTransparent(x, y - 1, z))
-                        {
-                            AddBottomFace(x, y, z, block);
-                        }
-                    }
-                }
+                instances[i] = new CombineInstance();
+                instances[i].mesh = sections[i].mesh;
+                Vector3 pos = transform.position;
+                pos.y += i * 16;
+                Quaternion rot = transform.rotation;
+                Vector3 scale = transform.localScale;
+                instances[i].transform = Matrix4x4.TRS(pos, rot, scale);
             }
-
-
-            //为点和index赋值
-            mesh.vertices = vertices.ToArray();
-            mesh.triangles = triangles.ToArray();
-            mesh.uv = uv.ToArray();
-
-            //重新计算顶点和法线
-            mesh.RecalculateBounds();
-            mesh.RecalculateNormals();
-
-            //将生成好的面赋值给组件
-            this.GetComponent<MeshFilter>().mesh = mesh;
+            //组合mesh
+            mesh.CombineMeshes(instances);
+            this.GetComponent<MeshFilter>().mesh =mesh;
             this.GetComponent<MeshCollider>().sharedMesh = mesh;
 
-            yield return null;
             isWorking = false;
         }
-
-        //此坐标方块是否透明，Chunk中的局部坐标
-        public bool IsBlockTransparent(int x, int y, int z)
+        void OnGUI()
         {
-            if (x >= width || y >= height || z >= width || x < 0 || y < 0 || z < 0)
-            {
-                return true;
-            }
-            else
-            {
-                //如果当前方块的id是0，那的确是透明的
-                return this.blocks[x, y, z] == 0;
-            }
+            GUILayout.Label("MeshVertexCount=" + mesh.vertexCount);
+            GUILayout.Label("MeshTriCount=" + mesh.triangles.LongLength);
         }
-
-
-
-        //前面
-        void AddFrontFace(int x, int y, int z, Block block)
-        {
-            //第一个三角面
-            triangles.Add(0 + vertices.Count);
-            triangles.Add(3 + vertices.Count);
-            triangles.Add(2 + vertices.Count);
-
-            //第二个三角面
-            triangles.Add(2 + vertices.Count);
-            triangles.Add(1 + vertices.Count);
-            triangles.Add(0 + vertices.Count);
-
-
-            //添加4个点
-            vertices.Add(new Vector3(0 + x, 0 + y, 0 + z));
-            vertices.Add(new Vector3(0 + x, 0 + y, 1 + z));
-            vertices.Add(new Vector3(0 + x, 1 + y, 1 + z));
-            vertices.Add(new Vector3(0 + x, 1 + y, 0 + z));
-
-            //添加UV坐标点，跟上面4个点循环的顺序一致
-            uv.Add(new Vector2(block.textureFrontX * textureOffset, block.textureFrontY * textureOffset) + new Vector2(shrinkSize, shrinkSize));
-            uv.Add(new Vector2(block.textureFrontX * textureOffset + textureOffset, block.textureFrontY * textureOffset) + new Vector2(-shrinkSize, shrinkSize));
-            uv.Add(new Vector2(block.textureFrontX * textureOffset + textureOffset, block.textureFrontY * textureOffset + textureOffset) + new Vector2(-shrinkSize, -shrinkSize));
-            uv.Add(new Vector2(block.textureFrontX * textureOffset, block.textureFrontY * textureOffset + textureOffset) + new Vector2(shrinkSize, -shrinkSize));
-        }
-
-        //背面
-        void AddBackFace(int x, int y, int z, Block block)
-        {
-            //第一个三角面
-            triangles.Add(0 + vertices.Count);
-            triangles.Add(3 + vertices.Count);
-            triangles.Add(2 + vertices.Count);
-
-            //第二个三角面
-            triangles.Add(2 + vertices.Count);
-            triangles.Add(1 + vertices.Count);
-            triangles.Add(0 + vertices.Count);
-
-
-            //添加4个点
-            vertices.Add(new Vector3(-1 + x, 0 + y, 1 + z));
-            vertices.Add(new Vector3(-1 + x, 0 + y, 0 + z));
-            vertices.Add(new Vector3(-1 + x, 1 + y, 0 + z));
-            vertices.Add(new Vector3(-1 + x, 1 + y, 1 + z));
-
-            //添加UV坐标点，跟上面4个点循环的顺序一致
-            uv.Add(new Vector2(block.textureBackX * textureOffset, block.textureBackY * textureOffset) + new Vector2(shrinkSize, shrinkSize));
-            uv.Add(new Vector2(block.textureBackX * textureOffset + textureOffset, block.textureBackY * textureOffset) + new Vector2(-shrinkSize, shrinkSize));
-            uv.Add(new Vector2(block.textureBackX * textureOffset + textureOffset, block.textureBackY * textureOffset + textureOffset) + new Vector2(-shrinkSize, -shrinkSize));
-            uv.Add(new Vector2(block.textureBackX * textureOffset, block.textureBackY * textureOffset + textureOffset) + new Vector2(shrinkSize, -shrinkSize));
-        }
-
-        //右面
-        void AddRightFace(int x, int y, int z, Block block)
-        {
-            //第一个三角面
-            triangles.Add(0 + vertices.Count);
-            triangles.Add(3 + vertices.Count);
-            triangles.Add(2 + vertices.Count);
-
-            //第二个三角面
-            triangles.Add(2 + vertices.Count);
-            triangles.Add(1 + vertices.Count);
-            triangles.Add(0 + vertices.Count);
-
-
-            //添加4个点
-            vertices.Add(new Vector3(0 + x, 0 + y, 1 + z));
-            vertices.Add(new Vector3(-1 + x, 0 + y, 1 + z));
-            vertices.Add(new Vector3(-1 + x, 1 + y, 1 + z));
-            vertices.Add(new Vector3(0 + x, 1 + y, 1 + z));
-
-            //添加UV坐标点，跟上面4个点循环的顺序一致
-            uv.Add(new Vector2(block.textureRightX * textureOffset, block.textureRightY * textureOffset) + new Vector2(shrinkSize, shrinkSize));
-            uv.Add(new Vector2(block.textureRightX * textureOffset + textureOffset, block.textureRightY * textureOffset) + new Vector2(-shrinkSize, shrinkSize));
-            uv.Add(new Vector2(block.textureRightX * textureOffset + textureOffset, block.textureRightY * textureOffset + textureOffset) + new Vector2(-shrinkSize, -shrinkSize));
-            uv.Add(new Vector2(block.textureRightX * textureOffset, block.textureRightY * textureOffset + textureOffset) + new Vector2(shrinkSize, -shrinkSize));
-        }
-
-        //左面
-        void AddLeftFace(int x, int y, int z, Block block)
-        {
-            //第一个三角面
-            triangles.Add(0 + vertices.Count);
-            triangles.Add(3 + vertices.Count);
-            triangles.Add(2 + vertices.Count);
-
-            //第二个三角面
-            triangles.Add(2 + vertices.Count);
-            triangles.Add(1 + vertices.Count);
-            triangles.Add(0 + vertices.Count);
-
-
-            //添加4个点
-            vertices.Add(new Vector3(-1 + x, 0 + y, 0 + z));
-            vertices.Add(new Vector3(0 + x, 0 + y, 0 + z));
-            vertices.Add(new Vector3(0 + x, 1 + y, 0 + z));
-            vertices.Add(new Vector3(-1 + x, 1 + y, 0 + z));
-
-            //添加UV坐标点，跟上面4个点循环的顺序一致
-            uv.Add(new Vector2(block.textureLeftX * textureOffset, block.textureLeftY * textureOffset) + new Vector2(shrinkSize, shrinkSize));
-            uv.Add(new Vector2(block.textureLeftX * textureOffset + textureOffset, block.textureLeftY * textureOffset) + new Vector2(-shrinkSize, shrinkSize));
-            uv.Add(new Vector2(block.textureLeftX * textureOffset + textureOffset, block.textureLeftY * textureOffset + textureOffset) + new Vector2(-shrinkSize, -shrinkSize));
-            uv.Add(new Vector2(block.textureLeftX * textureOffset, block.textureLeftY * textureOffset + textureOffset) + new Vector2(shrinkSize, -shrinkSize));
-        }
-
-        //上面
-        void AddTopFace(int x, int y, int z, Block block)
-        {
-            //第一个三角面
-            triangles.Add(1 + vertices.Count);
-            triangles.Add(0 + vertices.Count);
-            triangles.Add(3 + vertices.Count);
-
-            //第二个三角面
-            triangles.Add(3 + vertices.Count);
-            triangles.Add(2 + vertices.Count);
-            triangles.Add(1 + vertices.Count);
-
-
-            //添加4个点
-            vertices.Add(new Vector3(0 + x, 1 + y, 0 + z));
-            vertices.Add(new Vector3(0 + x, 1 + y, 1 + z));
-            vertices.Add(new Vector3(-1 + x, 1 + y, 1 + z));
-            vertices.Add(new Vector3(-1 + x, 1 + y, 0 + z));
-
-            //添加UV坐标点，跟上面4个点循环的顺序一致
-            uv.Add(new Vector2(block.textureTopX * textureOffset, block.textureTopY * textureOffset) + new Vector2(shrinkSize, shrinkSize));
-            uv.Add(new Vector2(block.textureTopX * textureOffset + textureOffset, block.textureTopY * textureOffset) + new Vector2(-shrinkSize, shrinkSize));
-            uv.Add(new Vector2(block.textureTopX * textureOffset + textureOffset, block.textureTopY * textureOffset + textureOffset) + new Vector2(-shrinkSize, -shrinkSize));
-            uv.Add(new Vector2(block.textureTopX * textureOffset, block.textureTopY * textureOffset + textureOffset) + new Vector2(shrinkSize, -shrinkSize));
-        }
-
-        //下面
-        void AddBottomFace(int x, int y, int z, Block block)
-        {
-            //第一个三角面
-            triangles.Add(1 + vertices.Count);
-            triangles.Add(0 + vertices.Count);
-            triangles.Add(3 + vertices.Count);
-
-            //第二个三角面
-            triangles.Add(3 + vertices.Count);
-            triangles.Add(2 + vertices.Count);
-            triangles.Add(1 + vertices.Count);
-
-
-            //添加4个点
-            vertices.Add(new Vector3(-1 + x, 0 + y, 0 + z));
-            vertices.Add(new Vector3(-1 + x, 0 + y, 1 + z));
-            vertices.Add(new Vector3(0 + x, 0 + y, 1 + z));
-            vertices.Add(new Vector3(0 + x, 0 + y, 0 + z));
-
-            //添加UV坐标点，跟上面4个点循环的顺序一致
-            uv.Add(new Vector2(block.textureBottomX * textureOffset, block.textureBottomY * textureOffset) + new Vector2(shrinkSize, shrinkSize));
-            uv.Add(new Vector2(block.textureBottomX * textureOffset + textureOffset, block.textureBottomY * textureOffset) + new Vector2(-shrinkSize, shrinkSize));
-            uv.Add(new Vector2(block.textureBottomX * textureOffset + textureOffset, block.textureBottomY * textureOffset + textureOffset) + new Vector2(-shrinkSize, -shrinkSize));
-            uv.Add(new Vector2(block.textureBottomX * textureOffset, block.textureBottomY * textureOffset + textureOffset) + new Vector2(shrinkSize, -shrinkSize));
-        }
+     
     }
 }
--- Assets/GameFramework/6.Map/Section.cs
+++ Assets/GameFramework/6.Map/Section.cs
@@ -5,18 +5,16 @@ using UniRx;
 using System.Linq;
 namespace GameFramework
 {
-    [RequireComponent(typeof(MeshFilter))]
-    [RequireComponent(typeof(MeshRenderer))]
-    [RequireComponent(typeof(MeshCollider))]
-    public class Section : MonoBehaviour
+    public class Section 
     {
         public static int width = 16;
         public static int height = 16;
-        public byte id;
+        public static int depth = 16;
+        public int id;
         public byte[,,] blocks;
         public Vector3Int position;
 
-        private Mesh mesh;
+        public Mesh mesh;
         //面需要的点
         private List<Vector3> vertices = new List<Vector3>();
         //生成三边面时用到的vertices的index
@@ -32,36 +30,29 @@ namespace GameFramework
         //当前Chunk是否正在生成中
         private bool isWorking = false;
 
-        void Start()
+        public Section()
         {
-            position = new Vector3Int((int)this.transform.position.x, (int)this.transform.position.y, (int)this.transform.position.z);
-            StartFunction();
+            
         }
-
-
-        void StartFunction()
+        public Section(int setId)
         {
-            mesh = new Mesh();
-            mesh.name = "Stection"+id;
-
-            StartCoroutine(CreateMap());
+            this.id = setId;
         }
-
-        IEnumerator CreateMap()
+        public  IEnumerator CreateMesh()
         {
             while (isWorking)
             {
                 yield return null;
             }
             isWorking = true;
-            blocks = new byte[width, height, width];
-            for (int x = 0; x < Chunk.width; x++)
+            blocks = new byte[width, height, depth];
+            for (int x = 0; x < Section.width; x++)
             {
-                for (int y = 0; y < Chunk.height; y++)
+                for (int y = 0; y < Section.height; y++)
                 {
-                    for (int z = 0; z < Chunk.width; z++)
+                    for (int z = 0; z < Section.depth; z++)
                     {
-                        if (y == Chunk.height - 1)
+                        if (y == Section.height - 1 && id==15)
                         {
                             if (Random.Range(1, 5) == 1)
                             {
@@ -75,21 +66,19 @@ namespace GameFramework
                     }
                 }
             }
-
-            StartCoroutine(CreateMesh());
-        }
-
-        IEnumerator CreateMesh()
-        {
+            //创建顶点、索引、uv
+           //初始化列表
+            mesh = new Mesh();
+            mesh.name = "Section"+id.ToString();
             vertices.Clear();
             triangles.Clear();
-
+            uv.Clear();
             //把所有面的点和面的索引添加进去
-            for (int x = 0; x < Chunk.width; x++)
+            for (int x = 0; x < Section.width; x++)
             {
-                for (int y = 0; y < Chunk.height; y++)
+                for (int y = 0; y < Section.height; y++)
                 {
-                    for (int z = 0; z < Chunk.width; z++)
+                    for (int z = 0; z < Section.depth; z++)
                     {
                         //获取当前坐标的Block对象
                         Block block = BlockList.GetBlock(this.blocks[x, y, z]);
@@ -122,22 +111,11 @@ namespace GameFramework
                     }
                 }
             }
-
-
             //为点和index赋值
             mesh.vertices = vertices.ToArray();
             mesh.triangles = triangles.ToArray();
             mesh.uv = uv.ToArray();
 
-            //重新计算顶点和法线
-            mesh.RecalculateBounds();
-            mesh.RecalculateNormals();
-
-            //将生成好的面赋值给组件
-            this.GetComponent<MeshFilter>().mesh = mesh;
-            this.GetComponent<MeshCollider>().sharedMesh = mesh;
-
-            yield return null;
             isWorking = false;
         }
 
