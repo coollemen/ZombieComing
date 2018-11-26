18441054a996bea5babe0432b7ec6cadd2098801
nizhihuahome <116778012@qq.com>
2018-11-26 21:24:29 +0800

    添加了maplayer编辑器

--- Assets/GameFramework/6.Map/Editor/MapEditor.cs
+++ Assets/GameFramework/6.Map/Editor/MapEditor.cs
@@ -14,15 +14,15 @@ namespace GameFramework
         SerializedProperty viewDepthProp;
         SerializedProperty chunkPrefabProp;
         SerializedProperty dataProp;
-        public string[] panelNames = new string[] {"Brushes","Blocks", "Chunks", "Config"};
-        public int selectPanelIndex = 0;
+        public string[] panelNames = new string[] {"Brushes", "Blocks", "Layers", "Config"};
+        public int selectPanelIndex =2;
         public string[] blockCreateModes = new string[] {"One Tex", "Two Tex", "Tree Tex", "Six Tex"};
         public int blockCreateModeIndex = 0;
         public BlockDefinition activeBlockDef;
         public Sprite top, bottom, left, right, front, back;
         public Vector2 blockScrollViewPos = Vector2.zero;
         public int selectBlockIndex = 0;
-
+        public int selectLayerIndex = 0;
         private void OnEnable()
         {
             this.widthProp = serializedObject.FindProperty("width");
@@ -32,18 +32,21 @@ namespace GameFramework
             this.chunkPrefabProp = serializedObject.FindProperty("chunkPrefab");
             this.dataProp = serializedObject.FindProperty("data");
         }
+
         private void OnDisable()
         {
-            Map map = (Map)target;
+            Map map = (Map) target;
             var data = map.data;
             EditorUtility.SetDirty(data);
         }
+
         private void OnDestroy()
         {
-            Map map = (Map)target;
+            Map map = (Map) target;
             var data = map.data;
             EditorUtility.SetDirty(data);
         }
+
         public override void OnInspectorGUI()
         {
             base.OnInspectorGUI();
@@ -60,31 +63,124 @@ namespace GameFramework
             selectPanelIndex = GUILayout.Toolbar(selectPanelIndex, panelNames);
             if (panelNames[selectPanelIndex] == "Blocks")
             {
-                this.DrawBlockEditPanel(data);
+                this.DrawBlocksPanel(data);
             }
-           else if (panelNames[selectPanelIndex] == "Chunks")
+            else if (panelNames[selectPanelIndex] == "Layers")
             {
+                this.DrawLayersPanel(data);
             }
             else if (panelNames[selectPanelIndex] == "Config")
             {
             }
-            //            for (int i = 0; i < data.blockDefinitions.Count; i++)
-            //            {
-            //                
-            //            }
+        }
+        /// <summary>
+        /// 绘制Layers页面
+        /// </summary>
+        /// <param name="data"></param>
+        public void DrawLayersPanel(MapData data)
+        {
+            EditorGUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+            MyGUITools.SetBackgroundColor(Color.green);
+            if (GUILayout.Button("Add New Layer"))
+            {
+                MapLayer layer = new MapLayer(0,255);
+                data.layers.Add(layer);
+
+            }
+            MyGUITools.RestoreBackgroundColor();
+            EditorGUILayout.EndHorizontal();
+            for (int i = 0; i < data.layers.Count; i++)
+            {
+                var layer = data.layers[i];
+                if (i == selectLayerIndex)
+                {
+                    EditorGUILayout.Space();
+                    EditorGUILayout.BeginVertical((GUIStyle)"SelectionRect");
+                    GUILayout.Toggle(false, string.Format("{0}:Start:{1}-End:{2}", i, layer.start, layer.end), (GUIStyle)"TL SelectionBarPreview", GUILayout.ExpandWidth(true));
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Label("Start");
+                    layer.start = EditorGUILayout.IntField( layer.start);
+                    GUILayout.Label("End  ");
+                    layer.end = EditorGUILayout.IntField( layer.end);
+                    GUILayout.EndHorizontal();
+                    EditorGUILayout.BeginHorizontal();
+                    GUILayout.FlexibleSpace();
+                    MyGUITools.SetBackgroundColor(Color.green);
+                    if (GUILayout.Button("Add New Item"))
+                    {
+                        MapLayerItem newItem = new MapLayerItem();
+                        layer.items.Add(newItem);
+                    }
+                    MyGUITools.RestoreBackgroundColor();
+                    EditorGUILayout.EndHorizontal();
+                    for (int j = 0; j < layer.items.Count; j++)
+                    {
+                        var item = layer.items[j];
+                        EditorGUILayout.BeginHorizontal();
+                        GUILayout.Label("Block");
+                        item.blockId= EditorGUILayout.IntPopup(item.blockId,GetBlockNames(data),GetBlockIDs(data));
+                        GUILayout.Label("Weight ");
+                        item.weight= EditorGUILayout.FloatField(item.weight);
+                        MyGUITools.SetBackgroundColor(Color.red);
+                        if (GUILayout.Button("Del"))
+                        {
+                            layer.items.RemoveAt(j);
+                        }
+                        MyGUITools.RestoreBackgroundColor();
+                        EditorGUILayout.EndHorizontal();
+                    }
+                    EditorGUILayout.EndVertical();
+                    EditorGUILayout.Space();
+                }
+                else
+                {
+                    var flag = GUILayout.Toggle(false, string.Format("{0}:Start:{1}-End:{2}", i,layer.start,layer.end), (GUIStyle)"OL Title");
+                    if (flag == true)
+                    {
+                        selectLayerIndex = i;
+                    }
+                }
+            }
         }
 
-        public void DrawBlockEditPanel(MapData data)
+        public int[] GetBlockIDs(MapData data)
         {
-            EditorGUILayout.PrefixLabel("Edit Block");
+            List<int> ids = new List<int>();
+            foreach (var d in data.blockDefinitions)
+            {
+                ids.Add(d.id);
+            }
+            return ids.ToArray();
+        }
+        public string[] GetBlockNames(MapData data)
+        {
+            List<string> names = new List<string>();
+            foreach (var d in data.blockDefinitions)
+            {
+                names.Add(d.name);
+            }
+            return names.ToArray();
+        }
+        /// <summary>
+        /// 绘制Blocks页面
+        /// </summary>
+        /// <param name="data"></param>
+        public void DrawBlocksPanel(MapData data)
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+            MyGUITools.SetBackgroundColor(Color.green);
             if (GUILayout.Button("Add New Block Definition"))
             {
                 BlockDefinition newdef = new BlockDefinition();
                 newdef.id = data.blockDefinitions.Count;
                 data.blockDefinitions.Add(newdef);
             }
+            MyGUITools.RestoreBackgroundColor();
+            GUILayout.EndHorizontal();
             //            EditorGUI.indentLevel++;
-            EditorGUILayout.PrefixLabel("Block Count("+data.blockDefinitions.Count+")");
+            GUILayout.Label("Block Count(" + data.blockDefinitions.Count + ")");
 
             var contents = new List<GUIContent>();
             for (int i = 0; i < data.blockDefinitions.Count; i++)
@@ -92,8 +188,10 @@ namespace GameFramework
                 var def = data.blockDefinitions[i];
                 if (i == selectBlockIndex)
                 {
-                    EditorGUILayout.BeginVertical((GUIStyle)"SelectionRect");
-                    GUILayout.Toggle(true,string.Format("ID:{0},Name:{1}", def.id, def.name), (GUIStyle)"TL SelectionBarPreview", GUILayout.ExpandWidth(true));
+                    EditorGUILayout.Space();
+                    EditorGUILayout.BeginVertical((GUIStyle) "SelectionRect");
+                    GUILayout.Toggle(true, string.Format("ID:{0},Name:{1}", def.id, def.name),
+                        (GUIStyle) "TL SelectionBarPreview", GUILayout.ExpandWidth(true));
                     def = data.blockDefinitions[i];
                     def.id = EditorGUILayout.IntField("ID", def.id);
                     def.name = EditorGUILayout.TextField("Name", def.name);
@@ -102,8 +200,8 @@ namespace GameFramework
                     blockCreateModeIndex = GUILayout.Toolbar(blockCreateModeIndex, blockCreateModes);
                     if (blockCreateModeIndex == 0)
                     {
-                       var temp = EditorGUILayout.ObjectField("All Face", def.top, typeof(Sprite), false) as Sprite;
-                        if (temp != null && temp!=def.top)
+                        var temp = EditorGUILayout.ObjectField("All Face", def.top, typeof(Sprite), false) as Sprite;
+                        if (temp != null && temp != def.top)
                         {
                             def.top = temp;
                             def.bottom = def.front = def.back = def.left = def.right = def.top;
@@ -112,8 +210,9 @@ namespace GameFramework
                     else if (blockCreateModeIndex == 1)
                     {
                         def.top = EditorGUILayout.ObjectField("Top Face", def.top, typeof(Sprite), false) as Sprite;
-                        var temp = EditorGUILayout.ObjectField("Other Face", def.bottom, typeof(Sprite), false) as Sprite;
-                        if (temp != null && temp!=def.bottom)
+                        var temp =
+                            EditorGUILayout.ObjectField("Other Face", def.bottom, typeof(Sprite), false) as Sprite;
+                        if (temp != null && temp != def.bottom)
                         {
                             def.bottom = temp;
                             def.front = def.back = def.left = def.right = def.bottom;
@@ -122,9 +221,10 @@ namespace GameFramework
                     else if (blockCreateModeIndex == 2)
                     {
                         def.top = EditorGUILayout.ObjectField("Top Face", def.top, typeof(Sprite), false) as Sprite;
-                        def.bottom = EditorGUILayout.ObjectField("Bottom Face", def.bottom, typeof(Sprite), false) as Sprite;
-                        var temp= EditorGUILayout.ObjectField("Other Face", def.front, typeof(Sprite), false) as Sprite;
-                        if (temp != null && temp!=def.front)
+                        def.bottom =
+                            EditorGUILayout.ObjectField("Bottom Face", def.bottom, typeof(Sprite), false) as Sprite;
+                        var temp = EditorGUILayout.ObjectField("Other Face", def.front, typeof(Sprite), false) as Sprite;
+                        if (temp != null && temp != def.front)
                         {
                             def.front = temp;
                             def.back = def.left = def.right = def.front;
@@ -133,39 +233,37 @@ namespace GameFramework
                     else if (blockCreateModeIndex == 3)
                     {
                         def.top = EditorGUILayout.ObjectField("Top Face", def.top, typeof(Sprite), false) as Sprite;
-                        def.bottom = EditorGUILayout.ObjectField("Bottom Face", def.bottom, typeof(Sprite), false) as Sprite;
-                        def.front = EditorGUILayout.ObjectField("Front Face", def.front, typeof(Sprite), false) as Sprite;
+                        def.bottom =
+                            EditorGUILayout.ObjectField("Bottom Face", def.bottom, typeof(Sprite), false) as Sprite;
+                        def.front =
+                            EditorGUILayout.ObjectField("Front Face", def.front, typeof(Sprite), false) as Sprite;
                         def.back = EditorGUILayout.ObjectField("Back Face", def.back, typeof(Sprite), false) as Sprite;
                         def.left = EditorGUILayout.ObjectField("Left Face", def.left, typeof(Sprite), false) as Sprite;
-                        def.right = EditorGUILayout.ObjectField("Right Face", def.right, typeof(Sprite), false) as Sprite;
+                        def.right =
+                            EditorGUILayout.ObjectField("Right Face", def.right, typeof(Sprite), false) as Sprite;
                     }
                     EditorGUILayout.BeginHorizontal();
                     GUILayout.FlexibleSpace();
-                    if (GUILayout.Button("Del",GUILayout.MinWidth(80)))
+                    MyGUITools.SetBackgroundColor(Color.red);
+                    if (GUILayout.Button("Del", GUILayout.MinWidth(80)))
                     {
                         data.blockDefinitions.RemoveAt(selectBlockIndex);
                     }
+                    MyGUITools.RestoreBackgroundColor();
                     EditorGUILayout.EndHorizontal();
                     EditorGUILayout.EndVertical();
+                    EditorGUILayout.Space();
                 }
                 else
                 {
-                    var flag = GUILayout.Toggle(false,string.Format("ID:{0},Name:{1}", def.id, def.name),(GUIStyle)"OL Title");
+                    var flag = GUILayout.Toggle(false, string.Format("ID:{0},Name:{1}", def.id, def.name),
+                        (GUIStyle) "OL Title");
                     if (flag == true)
                     {
                         selectBlockIndex = i;
                     }
                 }
-                //                contents.Add(new GUIContent(string.Format("ID:{0},Name:{1}", def.id, def.name)));
-
             }
-//            EditorGUI.indentLevel--;
-            //            var selected= GUILayout.SelectionGrid(selectBlockIndex, contents.ToArray(),1, (GUIStyle)"OL Title");
-            //            if (selected != selectBlockIndex)
-            //            {
-            //                selectBlockIndex = selected;
-            //                this.activeBlockDef = data.blockDefinitions[selectBlockIndex];
-            //            }
         }
-    }
+    }//end class
 }
---
+++ Assets/GameFramework/9.Tools/MyGUIColor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameFramework
+{
+    public class MyGUITools
+    {
+        public static Color oldColor;
+        public static Color bgOldColor;
+        public static Color contentOldColor;
+        public static void  SetColor(Color c)
+        {
+            oldColor = GUI.color;
+            GUI.color = c;
+        }
+        public static void SetContentColor(Color c)
+        {
+            contentOldColor = GUI.contentColor;
+            GUI.contentColor = c;
+        }
+        public static void SetBackgroundColor(Color c)
+        {
+            bgOldColor = GUI.backgroundColor;
+            GUI.backgroundColor = c;
+        }
+
+        public static void RestoreColor()
+        {
+            GUI.color = oldColor;
+        }
+        public static void RestoreBackgroundColor()
+        {
+            GUI.backgroundColor = bgOldColor;
+        }
+        public static void RestoreContentColor()
+        {
+            GUI.contentColor = contentOldColor;
+        }
+    }
+}
