77bdc8b9dd92f69b6a64db489829e9f29798daa2
nizhihuahome <116778012@qq.com>
2018-12-04 22:04:53 +0800

    完成新一轮map编辑器的代码重构
    为了更好的优化代码，减少下一步功能开发的复杂度，重构了map编辑器的绘制逻辑

--- Assets/GameFramework/6.Map/Editor/MapEditor.cs
+++ Assets/GameFramework/6.Map/Editor/MapEditor.cs
@@ -8,14 +8,9 @@ namespace GameFramework
     [CustomEditor(typeof(Map))]
     public class MapEditor : Editor
     {
-        SerializedProperty widthProp;
-        SerializedProperty depthProp;
-        SerializedProperty viewWidthProp;
-        SerializedProperty viewDepthProp;
-        SerializedProperty chunkPrefabProp;
-        SerializedProperty dataProp;
+
         public string[] panelNames = new string[] {"Brushes", "Blocks", "Layers", "Config"};
-        public int selectPanelIndex =2;
+        public int selectPanelIndex = 2;
         public string[] blockCreateModes = new string[] {"One Tex", "Two Tex", "Tree Tex", "Six Tex"};
         public int blockCreateModeIndex = 0;
         public BlockDefinition activeBlockDef;
@@ -23,21 +18,18 @@ namespace GameFramework
         public Vector2 blockScrollViewPos = Vector2.zero;
         public int selectBlockIndex = 0;
         public int selectLayerIndex = 0;
-        private List<Bounds> chunkBounds;
+//        private List<Bounds> chunkBounds;
+//        private List<Bounds> activeBlocksBounds;
         private bool isDirty = false;
-        private int selectedChunkIndex = -1;
+//        private int selectedChunkIndex = -1;
+//        private int selectedBlockIndex = -1;
         private bool isMouseIn = false;
         private Map map;
         private MapData data;
+
         private void OnEnable()
         {
-            this.widthProp = serializedObject.FindProperty("width");
-            this.depthProp = serializedObject.FindProperty("depth");
-            this.viewWidthProp = serializedObject.FindProperty("viewWidth");
-            this.viewDepthProp = serializedObject.FindProperty("viewDepth");
-            this.chunkPrefabProp = serializedObject.FindProperty("chunkPrefab");
-            this.dataProp = serializedObject.FindProperty("data");
-            map = (Map)target;
+            map = (Map) target;
             data = map.data;
         }
 
@@ -50,6 +42,7 @@ namespace GameFramework
         {
             EditorUtility.SetDirty(data);
         }
+
         /// <summary>
         /// 场景视图GUI绘制
         /// </summary>
@@ -57,43 +50,16 @@ namespace GameFramework
         {
             //如果map为null 不绘制
             if (map == null) return;
-            if (isDirty || this.chunkBounds==null)
-            {
-                this.CreateChunkBounds(map);
-                isDirty = false;
-            }
 //            this.DrawMapCube(map);
 //            this.DrawMapRectangle(map);
             this.DrawChunkRactangle(map);
 //            this.DrawChunkBounds(map);
             List<int> hitBlockBounds = new List<int>();
             Ray mouseRay = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
-            for (int i = 0; i < chunkBounds.Count; i++)
-            {
-                if (chunkBounds[i].IntersectRay(mouseRay))
-                {
-                    hitBlockBounds.Add(i);
-                }
-            }
-            if (hitBlockBounds.Count > 0)
-            {
-//                Debug.Log("hit chunk!");
-                float distance = 10000000;
-                int activeID = -1;
-                for (int i = 0; i < hitBlockBounds.Count; i++)
-                {
-                    var index = hitBlockBounds[i];
-                    float tempDistance = (mouseRay.origin -chunkBounds[index].center).magnitude;
-                    if (tempDistance < distance)
-                    {
-                        distance = tempDistance;
-                        activeID = index;
-                    }
-
-                }
-                this.selectedChunkIndex = activeID;
-            }
-            this.DrawActiveChunk(map);
+            var blockPoint = GetHitBlockMapPoint(mouseRay);
+            Handles.BeginGUI();
+            GUI.Label(new Rect(Event.current.mousePosition, new Vector2(100, 30)), blockPoint.ToString());
+            Handles.EndGUI();
             //绘制地图碰撞盒
             var mapBounds = this.GetMapBounds(map);
             //判读鼠标是不是在地图内
@@ -112,40 +78,114 @@ namespace GameFramework
                 if (Event.current.button == 0 && isMouseIn)
                 {
                     Debug.Log("Mouse Left Button Click!");
-                    if (this.selectedChunkIndex != -1 )
-                    {
-                        var go= map.transform.GetChild(0).gameObject;
-                        EditorGUIUtility.PingObject(go);
-                        Selection.activeGameObject = go;
-                        Event.current.Use();
-                    }
 
                 }
             }
         }
+
         #region SenceView相关函数
-        private void CreateBlockBounds()
+
+        /// <summary>
+        /// 获取鼠标所在block的地图点坐标
+        /// </summary>
+        /// <param name="mouseRay">鼠标射线</param>
+        /// <returns></returns>
+        public Vector3Int GetHitBlockMapPoint(Ray mouseRay)
         {
-            
+            var point = new Vector3Int();
+            //1、创建chunk的bounds，判断鼠标在哪个chunk
+            List<ChunkBounds> chunkBounds = this.CreateChunkBounds(map);
+            List<int> hitChunkBounds = new List<int>();
+            for (int i = 0; i < chunkBounds.Count; i++)
+            {
+
+                if (chunkBounds[i].bounds.IntersectRay(mouseRay))
+                {
+                    hitChunkBounds.Add(i);
+                }
+            }
+            if (hitChunkBounds.Count > 0)
+            {
+                //                Debug.Log("hit chunk!");
+                float distance = 10000000;
+                int activeID = -1;
+                for (int i = 0; i < hitChunkBounds.Count; i++)
+                {
+                    var index = hitChunkBounds[i];
+                    float tempDistance = (mouseRay.origin - chunkBounds[index].bounds.center).magnitude;
+                    if (tempDistance < distance)
+                    {
+                        distance = tempDistance;
+                        activeID = index;
+                    }
+
+                }
+                var hitChunk = chunkBounds[activeID];
+                this.DrawHitChunkBounds(hitChunk);
+                var blocksBounds = this.CreateActiveChunkBlocksBounds(hitChunk);
+                var hitBlock = this.CheckHitBlockBounds(mouseRay, blocksBounds);
+                if (hitBlock != null)
+                {
+                    this.DrawHitBlockBounds(hitBlock);
+                    return new Vector3Int(hitChunk.x * 16 + hitBlock.x, hitBlock.y, hitChunk.z * 16 + hitBlock.z);
+                }
+
+            }
+            //2、创建所在chunk的block的bounds，判断鼠标在哪个block
+            //3、转换为地图点坐标
+            return new Vector3Int(-1, -1, -1);
+
         }
 
-        private void CheckHitBlock()
+        private List<BlockBounds> CreateActiveChunkBlocksBounds(ChunkBounds activeChunkBounds)
         {
-            
+            var start = activeChunkBounds.bounds.center;
+            start.x = start.x - 8;
+            start.z = start.z - 8;
+
+            var blocksBounds = new List<BlockBounds>();
+            for (int j = 0; j < 16; j++)
+            {
+                for (int i = 0; i < 16; i++)
+                {
+                    var b = new Bounds();
+                    b.center = new Vector3(start.x + i + 0.5f, start.y, start.z + j + 0.5f);
+                    b.size = new Vector3(1, 1, 1);
+                    blocksBounds.Add(new BlockBounds(i, 0, j, b));
+                }
+            }
+            return blocksBounds;
         }
 
-        private void DrawActiveBlocks()
+        public void DrawHitBlockBounds(BlockBounds b)
         {
-            
+            var c = Handles.color;
+            Handles.color = Color.white;
+            Handles.DrawWireCube(b.bounds.center, b.bounds.size);
+            Handles.color = c;
+
         }
-        private void DrawActiveChunk(Map map)
+
+        public BlockBounds CheckHitBlockBounds(Ray mouseRay, List<BlockBounds> blocksBounds)
         {
-            if (this.selectedChunkIndex == -1) return;
-//            Debug.Log("Select Chunk Index=" + this.selectedChunkIndex);
-            var b = this.chunkBounds[this.selectedChunkIndex];
+            for (int i = 0; i < blocksBounds.Count; i++)
+            {
+                var b = blocksBounds[i].bounds;
+                if (b.IntersectRay(mouseRay))
+                {
+                    return blocksBounds[i];
+                }
+            }
+            return null;
+        }
+
+
+
+        private void DrawHitChunkBounds(ChunkBounds cb)
+        {
             var c = Handles.color;
             Handles.color = Color.green;
-            Handles.DrawWireCube(b.center, b.size);
+            Handles.DrawWireCube(cb.bounds.center, cb.bounds.size);
             Handles.color = c;
         }
 
@@ -156,49 +196,37 @@ namespace GameFramework
             Handles.DrawWireCube(b.center, b.size);
             Handles.color = c;
         }
+
         private Bounds GetMapBounds(Map map)
         {
             var pos = map.gameObject.transform.position;
 
-            var center = new Vector3(pos.x + map.width / 2 * 16,0.5f, pos.z + map.depth / 2 * 16);
+            var center = new Vector3(pos.x + map.width / 2 * 16, 0.5f, pos.z + map.depth / 2 * 16);
             var size = new Vector3(map.width * 16, 1, map.depth * 16);
-            Bounds b = new Bounds(center,size);
+            Bounds b = new Bounds(center, size);
             return b;
         }
-        private void CreateChunkBounds(Map map)
+
+        private List<ChunkBounds> CreateChunkBounds(Map map)
         {
-            this.chunkBounds = new List<Bounds>();
+            var chunksBounds = new List<ChunkBounds>();
             var pos = map.gameObject.transform.position;
+
             for (int j = 0; j < map.depth; j++)
             {
                 for (int i = 0; i < map.width; i++)
                 {
-
                     Bounds b = new Bounds();
                     b.center = new Vector3(pos.x + i * 16 + 8,
                         pos.y + 0.5f,
                         pos.z + j * 16 + 8);
                     //一层的高度，一个chunk的宽度和深度
                     b.size = new Vector3(16, 1, 16);
-                    chunkBounds.Add(b);
+                    chunksBounds.Add(new ChunkBounds(i, j, b));
                 }
-            }
-        }
 
-        private void DrawChunkBounds(Map map)
-        {
-            for (int i = 0; i < chunkBounds.Count; i++)
-            {
-                var b = chunkBounds[i];
-                var c = Handles.color;
-                Handles.color = Color.green;
-                Handles.DrawWireCube(b.center, b.size);
-                Handles.color = c;
             }
-        }
-        private void CheckChunkBounds()
-        {
-            
+            return chunksBounds;
         }
 
         private void DrawChunkRactangle(Map map)
@@ -236,12 +264,12 @@ namespace GameFramework
             Vector3[] verts = new Vector3[]
             {
                 new Vector3(pos.x, pos.y, pos.z),
-                new Vector3(pos.x+map.width*16, pos.y, pos.z),
-                new Vector3(pos.x+map.width*16, pos.y, pos.z+map.depth*16),
-                new Vector3(pos.x, pos.y, pos.z+map.depth*16),
+                new Vector3(pos.x + map.width * 16, pos.y, pos.z),
+                new Vector3(pos.x + map.width * 16, pos.y, pos.z + map.depth * 16),
+                new Vector3(pos.x, pos.y, pos.z + map.depth * 16),
 
             };
-            Handles.DrawSolidRectangleWithOutline(verts,new Color(1f,1f,1f,0.1f), new Color(0f, 1f, 0f, 1f));
+            Handles.DrawSolidRectangleWithOutline(verts, new Color(1f, 1f, 1f, 0.1f), new Color(0f, 1f, 0f, 1f));
         }
 
         private void DrawMapCube(Map map)
@@ -274,22 +302,39 @@ namespace GameFramework
                 }
             }
         }
-        #endregion
-# region MapData操作函数
 
+        #endregion
+
+        #region MapData操作函数
+
+        /// <summary>
+        /// 获取地图某个Block的数据
+        /// </summary>
+        /// <param name="x">地图X</param>
+        /// <param name="y">地图Y</param>
+        /// <param name="z">地图Z</param>
+        /// <returns>byte类型数据</returns>
         public byte GetBlockFromData(int x, int y, int z)
         {
             Vector2Int chunkIndex = GetChunkIndexFromMapPoint(x, z);
             Vector3Int chunkPoint = GetChunkPointFromMapPoint(x, y, z);
-            return data.chunkDatas[chunkIndex.x][chunkIndex.y][chunkPoint.x,chunkPoint.y,chunkPoint.z];
+            return data.chunkDatas[chunkIndex.x][chunkIndex.y][chunkPoint.x, chunkPoint.y, chunkPoint.z];
         }
 
+        /// <summary>
+        /// 设置地图某个Block的数据
+        /// </summary>
+        /// <param name="x">地图X</param>
+        /// <param name="y">地图Y</param>
+        /// <param name="z">地图Z</param>
+        /// <param name="blockData">数据</param>
         public void SetBlockToData(int x, int y, int z, byte blockData)
         {
             Vector2Int chunkIndex = GetChunkIndexFromMapPoint(x, z);
             Vector3Int chunkPoint = GetChunkPointFromMapPoint(x, y, z);
             data.chunkDatas[chunkIndex.x][chunkIndex.y][chunkPoint.x, chunkPoint.y, chunkPoint.z] = blockData;
         }
+
         /// <summary>
         /// 将地图坐标转换为Chunk的数组索引
         /// </summary>
@@ -299,9 +344,10 @@ namespace GameFramework
         private Vector2Int GetChunkIndexFromMapPoint(int mapX, int mapZ)
         {
             int x = Mathf.FloorToInt(mapX / data.width);
-            int z = Mathf.FloorToInt(mapZ/ data.depth);
+            int z = Mathf.FloorToInt(mapZ / data.depth);
             return new Vector2Int(x, z);
         }
+
         /// <summary>
         /// 将地图坐标转换为在Chunk中的局部坐标
         /// </summary>
@@ -315,7 +361,9 @@ namespace GameFramework
             int z = mapZ % data.depth;
             return new Vector3Int(x, mapY, z);
         }
-#endregion
+
+        #endregion
+
         public override void OnInspectorGUI()
         {
             base.OnInspectorGUI();
@@ -331,10 +379,10 @@ namespace GameFramework
 //            GUILayout.EndHorizontal();
             if (GUILayout.Button("Create Map"))
             {
-               //创建map的chunks
+                //创建map的chunks
                 for (int i = 0; i < map.transform.childCount; i++)
                 {
-                    DestroyImmediate(map.transform.GetChild(i).gameObject); 
+                    DestroyImmediate(map.transform.GetChild(i).gameObject);
                 }
                 map.CreateMapByEditor();
             }
@@ -359,15 +407,18 @@ namespace GameFramework
             {
             }
         }
+
         #region OnInspectorGUI相关函数
+
         /// <summary>
         /// 绘制笔刷页面
         /// </summary>
         /// <param name="data"></param>
         public void DrawBrushesPanel(MapData data)
         {
-            
+
         }
+
         /// <summary>
         /// 绘制Layers页面
         /// </summary>
@@ -379,7 +430,7 @@ namespace GameFramework
             MyGUITools.SetBackgroundColor(Color.green);
             if (GUILayout.Button("Add New Layer"))
             {
-                MapLayer layer = new MapLayer(0,255);
+                MapLayer layer = new MapLayer(0, 255);
                 data.layers.Add(layer);
 
             }
@@ -391,13 +442,14 @@ namespace GameFramework
                 if (i == selectLayerIndex)
                 {
                     EditorGUILayout.Space();
-                    EditorGUILayout.BeginVertical((GUIStyle)"SelectionRect");
-                    GUILayout.Toggle(false, string.Format("{0}:Start:{1}-End:{2}", i, layer.start, layer.end), (GUIStyle)"TL SelectionBarPreview", GUILayout.ExpandWidth(true));
+                    EditorGUILayout.BeginVertical((GUIStyle) "SelectionRect");
+                    GUILayout.Toggle(false, string.Format("{0}:Start:{1}-End:{2}", i, layer.start, layer.end),
+                        (GUIStyle) "TL SelectionBarPreview", GUILayout.ExpandWidth(true));
                     GUILayout.BeginHorizontal();
                     GUILayout.Label("Start");
-                    layer.start = EditorGUILayout.IntField( layer.start);
+                    layer.start = EditorGUILayout.IntField(layer.start);
                     GUILayout.Label("End  ");
-                    layer.end = EditorGUILayout.IntField( layer.end);
+                    layer.end = EditorGUILayout.IntField(layer.end);
                     GUILayout.EndHorizontal();
                     EditorGUILayout.BeginHorizontal();
                     GUILayout.FlexibleSpace();
@@ -414,9 +466,9 @@ namespace GameFramework
                         var item = layer.items[j];
                         EditorGUILayout.BeginHorizontal();
                         GUILayout.Label("Block");
-                        item.blockId= EditorGUILayout.IntPopup(item.blockId,GetBlockNames(data),GetBlockIDs(data));
+                        item.blockId = EditorGUILayout.IntPopup(item.blockId, GetBlockNames(data), GetBlockIDs(data));
                         GUILayout.Label("Weight ");
-                        item.weight= EditorGUILayout.FloatField(item.weight);
+                        item.weight = EditorGUILayout.FloatField(item.weight);
                         MyGUITools.SetBackgroundColor(Color.red);
                         if (GUILayout.Button("Del"))
                         {
@@ -430,7 +482,8 @@ namespace GameFramework
                 }
                 else
                 {
-                    var flag = GUILayout.Toggle(false, string.Format("{0}:Start:{1}-End:{2}", i,layer.start,layer.end), (GUIStyle)"OL Title");
+                    var flag = GUILayout.Toggle(false, string.Format("{0}:Start:{1}-End:{2}", i, layer.start, layer.end),
+                        (GUIStyle) "OL Title");
                     if (flag == true)
                     {
                         selectLayerIndex = i;
@@ -448,6 +501,7 @@ namespace GameFramework
             }
             return ids.ToArray();
         }
+
         public string[] GetBlockNames(MapData data)
         {
             List<string> names = new List<string>();
@@ -457,6 +511,7 @@ namespace GameFramework
             }
             return names.ToArray();
         }
+
         /// <summary>
         /// 绘制Blocks页面
         /// </summary>
@@ -560,6 +615,47 @@ namespace GameFramework
                 }
             }
         }
+
         #endregion
-    }//end class
+    } //end class
+
+    public class ChunkBounds
+    {
+        public int x;
+        public int z;
+        public Bounds bounds;
+
+        public ChunkBounds()
+        {
+            bounds = new Bounds();
+        }
+
+        public ChunkBounds(int setX, int setZ, Bounds setBounds)
+        {
+            x = setX;
+            z = setZ;
+            bounds = setBounds;
+        }
+    }
+
+    public class BlockBounds
+    {
+        public int x;
+        public int y;
+        public int z;
+        public Bounds bounds;
+
+        public BlockBounds()
+        {
+            bounds = new Bounds();
+        }
+
+        public BlockBounds(int setX, int setY, int setZ, Bounds setBounds)
+        {
+            x = setX;
+            y = setY;
+            z = setZ;
+            bounds = setBounds;
+        }
+    }
 }
